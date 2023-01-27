9e173ec6677b1d5aa2c06feee369adcf142f8177
FrancoModesto <franco.modesto@hotmail.com>
2023-01-27 07:42:22 -0300

    Agregado Menú Hamburguesa

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,26 @@
+//HAMBURGUER MENU
+if (window.screen.width < 800) {
+    const menuHbtn = document.querySelector(".menuH-btn")
+    const nav = document.querySelector("nav")
+    const menu = document.querySelector("nav ul")
+
+    let on = false
+    nav.addEventListener("click", () => {
+        if (on) {
+            nav.style.height = "4em"
+            menu.style.top = "-50vh"
+            menuHbtn.style.opacity = "1"
+            on = false
+        } else {
+            nav.style.height = "50vh"
+            menu.style.top = "0"
+            menuHbtn.style.opacity = "0"
+            on = true
+        }
+    })
+}
+
+
 //PARALLAX
 const parallax = document.querySelector(".parallax")
 const profile = document.querySelector(".profile")
